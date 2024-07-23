a179553cafa19049df04b84a3930c07b22e99e83
mossab idbenkellal <midbenke@noreply.learn.zone01oujda.ma>
2024-07-23 15:50:24 +0000

    Update 'printcomb2.go'

--- printcomb2.go
+++ printcomb2.go
@@ -13,7 +13,7 @@ func PrintComb2() {
 						z01.PrintRune(' ')
 						z01.PrintRune(k)
 						z01.PrintRune(l)
-						if i != '9' || j != '8' || k != '9' || l !='9' {
+						if i != '9' || j != '8' || k != '9' || l != '9' {
 							z01.PrintRune(',')
 							z01.PrintRune(' ')
 						}
