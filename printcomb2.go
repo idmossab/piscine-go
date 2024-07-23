34f23dab91a962510668b99bb0d7370844fe4818
Mossab Idbenkellal <idmossab@gmail.com>
2024-07-23 16:09:25 +0100

    print-comb2

--- printcomb2.go
+++ printcomb2.go
@@ -3,11 +3,11 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
-	for i:= '0'; i<='9'; i++ {
-		for j:= '0'; j<='9'; j++ {
-			for k:= '0'; k<='9'; k++ {
-				for l:= '1'; l<='9'; l++ {
-					if i<=k && j<l{
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j<='9'; j++ {
+			for k := '0'; k<='9'; k++ {
+				for l := '1'; l <='9'; l++ {
+					if i <= k && j < l {
 					z01.PrintRune(i)
 					z01.PrintRune(j)
 					z01.PrintRune(' ')
