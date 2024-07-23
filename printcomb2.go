c3a60f315d3e225e6c5bd285bd55be9a4920e128
mossab idbenkellal <midbenke@noreply.learn.zone01oujda.ma>
2024-07-23 15:59:55 +0000

    Update 'printcomb2.go'

--- printcomb2.go
+++ printcomb2.go
@@ -6,8 +6,8 @@ func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
 			for k := '0'; k <= '9'; k++ {
-				for l := '1'; l <= '9'; l++ {
-					if i <= k && j < l {
+				for l := '0'; l <= '9'; l++ {
+					if i < k || ( i == k && j < l ) {
 						z01.PrintRune(i)
 						z01.PrintRune(j)
 						z01.PrintRune(' ')
