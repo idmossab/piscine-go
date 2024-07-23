445d4f7b4ee8606e469352a5da49445123327631
Mossab Idbenkellal <idmossab@gmail.com>
2024-07-23 18:01:53 +0100

    printnbr

---
+++ printnbr.go
@@ -0,0 +1,24 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbr(n int) {
+	if n < 0 {
+		z01.PrintRune('-')
+		n *= -1
+	}
+	if n == 0 {
+		z01.PrintRune('0')
+	}
+
+	res :=""
+
+	for n > 0 {
+		res += string(n%10)
+		n = n/10
+	}
+	for i:=len(res)-1 ;i>=0;i--{
+
+		z01.PrintRune(rune(res[i]+'0'))
+	}
+}
---
+++ test/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"piscine"
+	"github.com/01-edu/z01"
+)
+func main() {
+	piscine.PrintNbr(-123)
+	piscine.PrintNbr(0)
+	piscine.PrintNbr(123)
+	piscine.PrintNbr(-941521321245231002)
+	z01.PrintRune('\n')
+}
