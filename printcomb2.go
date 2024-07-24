abce65fdfa2420007c9356b033ce0014e7a7ffeb
Mossab Idbenkellal <idmossab@gmail.com>
2024-07-24 17:00:05 +0100

    test1

--- printcomb2.go
+++ printcomb2.go
@@ -7,7 +7,7 @@ func PrintComb2() {
 		for j := '0'; j <= '9'; j++ {
 			for k := '0'; k <= '9'; k++ {
 				for l := '0'; l <= '9'; l++ {
-					if i < k || (i ==k && j < l) {
+					if i < k || (i == k && j < l) {
 						z01.PrintRune(i)
 						z01.PrintRune(j)
 						z01.PrintRune(' ')
--- printnbr.go
+++ printnbr.go
@@ -5,7 +5,7 @@ import "github.com/01-edu/z01"
 func PrintNbr(n int) {
 	if n < 0 {
 		z01.PrintRune('-')
-		n *=-1
+		n *= -1
 	}
 	if n == 0 {
 		z01.PrintRune('0')
