030299707dc4460b6d82a78113db22143b424de6
Christopher  Fremond <dev@01-edu.org>
2024-07-26 10:08:49 +0000

    Update 'atoi.go'

--- atoi.go
+++ atoi.go
@@ -4,20 +4,20 @@ func Atoi(s string) int {
 	result := 0
 	sign := 1
 	for i := 0; i < len(s); i++ {
+		// condition 2:
+		if i == 0 && (s[0] == '-' || s[0] == '+') {
+			if s[0] == '-' {
+				sign *= -1
+			}
+			continue
+		}
 		// condition 1:
 		if s[i] < '0' || s[i] > '9' {
-			// condition 2:
-			if i == 0 && (s[0] == '-' || s[0] == '+') {
-				if s[0] == '-' {
-					sign *= -1
-				}
-				continue
-			}
 			return 0
 		}
 		dig := int(rune(s[i] - '0'))
-		result = result*10*sign + dig
+		result = result*10 + dig
 	}
 
-	return result
+	return result * sign
 }
