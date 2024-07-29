0e72cb921a60ee5551b1c29110fb3465b9965146
Mossab Idbenkellal <idmossab@gmail.com>
2024-07-29 14:46:31 +0100

    factorial1

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,8 +1,11 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb <= 0 {
+	if nb < 0 || nb>=20{
 		return 0
+	}
+	if nb == 0 {
+		return 1
 	}
 	res := 1
 	for i := 1; i <= nb; i++ {
