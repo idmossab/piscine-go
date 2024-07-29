509b2e067f4f70cf6de0267ca64ddde0e2490196
Mossab Idbenkellal <idmossab@gmail.com>
2024-07-29 16:13:59 +0100

    power

--- iterativepower.go
+++ iterativepower.go
@@ -4,7 +4,7 @@ func IterativePower(nb int, power int) int {
 	if power == 0 || nb == 1 {
 		return 1
 	}
-	if (nb < 0 && power < 0) || (nb > 0 && power < 0) {
+	if power < 0 && (nb < 0 || nb > 0) {
 		return 0
 	}
 	res := 1
