369863354e67a24813783350e8d5688a262d531e
Mossab Idbenkellal <idmossab@gmail.com>
2024-07-30 18:59:49 +0100

    quest5

--- strrev.go
+++
@@ -1,9 +0,0 @@
-package piscine
-
-func StrRev(s string) string {
-	rev := ""
-	for i := len(s) - 1; i >= 0; i-- {
-		rev += string(s[i])
-	}
-	return rev
-}
--- ultimatedivmod.go
+++
@@ -1,7 +0,0 @@
-package piscine
-
-func UltimateDivMod(a *int, b *int) {
-	c := *a
-	*a = *a / *b
-	*b = c % *b
-}
