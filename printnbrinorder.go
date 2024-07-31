4997fe911ec64d5d39563a0501db3d9a4a5f38f6
mossab <idmossab@gmail.com>
2024-07-31 02:06:02 +0100

    printorder

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -4,17 +4,25 @@ import "github.com/01-edu/z01"
 
 func PrintNbrInOrder(n int) {
 	if n < 0 {
-		n *= -1
+		return
 	}
 	if n == 0 {
 		z01.PrintRune('0')
+		return
 	}
 	res := ""
 	for n > 0 {
-		res += string(n % 10)
+		res += string(rune(n%10 + '0'))
 		n /= 10
 	}
-	for i := 0; i < len(res); i++ {
-		z01.PrintRune(rune(res[i] + '0'))
+	runes := []rune(res)
+	for i := 0; i < len(runes)-1; i++ {
+		if runes[i] > runes[i+1] {
+			runes[i], runes[i+1] = runes[i+1], runes[i]
+			i = -1
+		}
+	}
+	for i := 0; i < len(runes); i++ {
+		z01.PrintRune(runes[i])
 	}
 }
