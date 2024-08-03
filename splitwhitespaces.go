5721f3777f87867ab6103717580f4667b8d99ef7
mossab <idmossab@gmail.com>
2024-08-03 17:07:54 +0100

    splitt

---
+++ splitwhitespaces.go
@@ -0,0 +1,15 @@
+package piscine
+func SplitWhiteSpaces(s string) []string {
+	var table []string
+	res := ""
+	for i := 0; i < len(s); i++ {
+		if s[i]!=' '{
+			res+=string(s[i])
+		}else if res !="" {
+			table = append(table, res)
+			res = ""
+		}
+	}
+	table = append(table, res)
+	return table
+}
