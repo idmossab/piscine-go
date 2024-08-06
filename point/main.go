620691922a25ff80644f088ab07550843486122f
mossab <idmossab@gmail.com>
2024-08-06 18:59:51 +0100

    prtpoint2

--- point/main.go
+++ point/main.go
@@ -14,20 +14,16 @@ func setPoint(ptr *point) {
 
 func main() {
 	points := &point{}
-
 	setPoint(points)
-
-	z01.PrintRune('x')
-	z01.PrintRune(' ')
-	z01.PrintRune('=')
-	z01.PrintRune(' ')
+	str1 := "x = "
+	for _, r := range str1 {
+		z01.PrintRune(r)
+	}
 	PrtPoint(points.x)
-	z01.PrintRune(',')
-	z01.PrintRune(' ')
-	z01.PrintRune('y')
-	z01.PrintRune(' ')
-	z01.PrintRune('=')
-	z01.PrintRune(' ')
+	str2 := ", y = "
+	for _, r1 := range str2 {
+		z01.PrintRune(r1)
+	}
 	PrtPoint(points.y)
 	z01.PrintRune('\n')
 }
