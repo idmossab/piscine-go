276a160e562b86c5d777e4e26ded7b70d8026f25
Christopher  Fremond <dev@01-edu.org>
2024-08-12 15:46:34 +0000

    Update 'listsize.go'

--- listsize.go
+++ listsize.go
@@ -1,9 +1,19 @@
 package piscine
 
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
 func ListSize(l *List) int {
   size:=0
-  for i:=l.Head ;i<l.Tail;i++{
-    res++
+  for i:=l.Head ;i!=l.Tail;i=i.Next{
+    size++
   }
   return size
 }
